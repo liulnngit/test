4bed1b7e3b96c23df5ecfe0e00556e52fc8ff333
410524892 <410524892@qq.com>
2019-03-14 09:32:53 +0800

    easyexcel 案例

--- excel-demo/src/test/java/com/jesse/excel/test/ExcelTest.java
+++ excel-demo/src/test/java/com/jesse/excel/test/ExcelTest.java
@@ -1,5 +1,6 @@
 package com.jesse.excel.test;
 
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -12,6 +13,7 @@ import java.util.List;
 
 import org.junit.Test;
 
+import com.alibaba.excel.EasyExcelFactory;
 import com.alibaba.excel.ExcelReader;
 import com.alibaba.excel.ExcelWriter;
 import com.alibaba.excel.context.AnalysisContext;
@@ -19,21 +21,66 @@ import com.alibaba.excel.event.AnalysisEventListener;
 import com.alibaba.excel.metadata.Sheet;
 import com.alibaba.excel.support.ExcelTypeEnum;
 import com.jesse.excel.ExcelListener;
+import com.jesse.excel.domain.ExcelPropertyIndexModel;
 import com.jesse.excel.domain.ImportInfo;
+import com.jesse.excel.domain.MultiLineHeadExcelModel;
 
 public class ExcelTest {
 
 	@Test
 	public void testRead() throws FileNotFoundException {
-		InputStream inputStream = getInputStream("H:\\mytest\\test.xls");
+		InputStream inputStream = getInputStream("H:\\mytest\\20181231-G0100-181-境内汇总数据-月-人民币.xls");
 		try {
 			
 			// 解析每行结果在listener中处理
             ExcelListener listener = new ExcelListener();
 			
+			ExcelReader reader = EasyExcelFactory.getReader(inputStream, new AnalysisEventListener() {
+				@Override
+				public void invoke(Object o, AnalysisContext analysisContext) {
+					Sheet sheet = analysisContext.getCurrentSheet();
+					System.out.println(analysisContext.getCurrentRowNum());
+					System.out.println("当前sheet" + analysisContext.getCurrentSheet().getSheetNo() + " 当前行："
+							+ analysisContext.getCurrentRowNum() + " data:" + o);
+				}
+
+				@Override
+				public void doAfterAllAnalysed(AnalysisContext analysisContext) {
+					Sheet sheet = analysisContext.getCurrentSheet();
+					//sheet.setTableStyle(tableStyle);
+					System.out.println(sheet.getColumnWidthMap());
+					System.out.println(sheet.getTableStyle());
+				}
+			});
+			List<Sheet> sheets = reader.getSheets();
+	        System.out.println("llll****"+sheets);
+	        System.out.println();
+	        for (Sheet sheet:sheets) {
+	            if(sheet.getSheetNo() ==1) {
+	                reader.read(sheet);
+	            }
+	        }
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				inputStream.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	@Test
+	public void testRead2() throws FileNotFoundException {
+		InputStream inputStream = getInputStream("H:\\mytest\\test.xls");
+		try {
+			
 			ExcelReader reader = new ExcelReader(inputStream, ExcelTypeEnum.XLS, null, new AnalysisEventListener() {
 				@Override
 				public void invoke(Object o, AnalysisContext analysisContext) {
+					Sheet sheet = analysisContext.getCurrentSheet();
+					System.out.println(analysisContext.getCurrentRowNum());
 					System.out.println("当前sheet" + analysisContext.getCurrentSheet().getSheetNo() + " 当前行："
 							+ analysisContext.getCurrentRowNum() + " data:" + o);
 				}
@@ -54,7 +101,8 @@ public class ExcelTest {
 			}
 		}
 	}
-
+	
+	
 	@Test
 	public void testWriter() throws FileNotFoundException {
 		OutputStream out = new FileOutputStream("H:\\mytest\\test-OUT.xls");
@@ -62,8 +110,8 @@ public class ExcelTest {
 			ExcelWriter writer = new ExcelWriter(out, ExcelTypeEnum.XLS);
 			// 写第一个sheet
 			Sheet sheet = new Sheet(2, 3, ImportInfo.class);
-			writer.write(getDate(), sheet);
-			for (ImportInfo in : getDate()) {
+			writer.write(getData(), sheet);
+			for (ImportInfo in : getData()) {
 				System.out.println(in.getName());
 			}
 			writer.finish();
@@ -78,7 +126,7 @@ public class ExcelTest {
 		}
 	}
 
-	public List<ImportInfo> getDate() {
+	public List<ImportInfo> getData() {
 		List<ImportInfo> list = new ArrayList<ImportInfo>();
 		ImportInfo info = new ImportInfo();
 		info.setAge(12);
@@ -100,11 +148,51 @@ public class ExcelTest {
 
 	private InputStream getInputStream(String fileName) {
 		try {
-			return new FileInputStream(new File(fileName));
+			return new BufferedInputStream(new FileInputStream(fileName));
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
-
+	
+	@Test
+    public void test1() throws FileNotFoundException {
+        OutputStream out = new FileOutputStream("H:\\mytest\\test-OUT.xls");
+        try {
+            ExcelWriter writer = new ExcelWriter(out, ExcelTypeEnum.XLSX);
+            //写第一个sheet, sheet1  数据全是List<String> 无模型映射关系
+            Sheet sheet1 = new Sheet(1, 0,ExcelPropertyIndexModel.class);
+            writer.write(getData(), sheet1);
+            writer.finish();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+	
+	@Test
+    public void testMulti() throws FileNotFoundException {
+        OutputStream out = new FileOutputStream("H:\\mytest\\test-multi.xls");
+        try {
+            ExcelWriter writer = new ExcelWriter(out, ExcelTypeEnum.XLSX);
+            //写第一个sheet, sheet1  数据全是List<String> 无模型映射关系
+            Sheet sheet1 = new Sheet(1, 0,MultiLineHeadExcelModel.class);
+            writer.write(getData(), sheet1);
+            writer.finish();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+	
 }
